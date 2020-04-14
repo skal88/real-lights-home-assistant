f6cbd17caf19f4e7778f30ccf7d1cd7ed02ff738
Albert Mulà <albertmulac@gmail.com>
2020-04-14 03:12:23 +0200

    First commit

---
+++ const.py
@@ -0,0 +1,8 @@
+"""DEFINE cosntants for the Real Lights integration."""
+
+DOMAIN = "real_lights"
+
+CONF_ENTITIES = "entities"
+CONF_SENSOR_NAME = "sensor_name"
+
+DEFAULT_SENSOR_NAME = "real_lights"
